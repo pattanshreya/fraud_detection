6904a9f3a3c7518f9c2cf0298f25e1a4637aeec9
pattanshreya <pattanshreya30@gmail.com>
2024-05-14 21:20:31 +0530

    correct code

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,14 @@
 
 print("adding all data processing files")
+
+a=5
+b=3
+def sum(a,b):
+    c=a+b
+    return c
+print("addition")
+
+def multi(a,b):
+    d=a*b
+    return d
+print("multiplication")
