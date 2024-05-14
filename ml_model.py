e749300a490d5ec8e80cc50afd68fe220d5a27da
pattanshreya <pattanshreya30@gmail.com>
2024-05-14 21:40:19 +0530

    added 3 ml model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all ml related code")
+
+def model1():
+    print("adding all ml model1")
+
+def model2():
+    print("adding all ml model2")
+
+def model3():
+    print("adding all ml model3")
